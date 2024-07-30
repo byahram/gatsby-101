fcb83f95561b299084d4fbc099352f7e7806e833
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-07-30 17:49:41 +0900

    #10.10 Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/SEO";
+import { graphql, PageProps } from "gatsby";
 
-export default function Blog() {
-  const [blogPosts, setBlogPosts] = useState();
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
+  console.log(data);
 
+  //   const [blogPosts, setBlogPosts] = useState();
   //   useEffect(() => {
   //     fetchBlogPosts();
   //   }, []);
 
   return (
     <Layout title="Blog">
-      <p>The most recent news from my shop.</p>
-      {blogPosts}
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
